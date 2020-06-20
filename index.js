9c5a5a73db11008ded5a954dd31b09579b2b3477
msyk <nii@msyk.net>
2020-06-20 13:23:46 +0900

    1.0.3, bugfix for INTER-Mediator v6.

--- index.js
+++ index.js
@@ -9,93 +9,93 @@
  */
 
 var tinymceOption = {
-    theme: 'silver',
-    width: 800,
-    height: 400,
-    plugins: ['advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker',
-        'searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking',
-        'save table directionality emoticons template paste'],
-    // content_css: 'css/content.css',
-    toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify ' +
-        '| bullist numlist outdent indent | link image | print preview media fullpage ' +
-        '| forecolor backcolor emoticons',
-    style_formats: [
-        {title: 'Bold text', inline: 'b'},
-        {title: 'Red text', inline: 'span', styles: {color: '#ff0000'}},
-        {title: 'Red header', block: 'h1', styles: {color: '#ff0000'}},
-        {title: 'Example 1', inline: 'span', classes: 'example1'},
-        {title: 'Example 2', inline: 'span', classes: 'example2'},
-        {title: 'Table styles'},
-        {title: 'Table row 1', selector: 'tr', classes: 'tablerow1'}
-    ]
+  theme: 'silver',
+  width: 800,
+  height: 400,
+  plugins: ['advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker',
+    'searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking',
+    'save table directionality emoticons template paste'],
+  // content_css: 'css/content.css',
+  toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify ' +
+    '| bullist numlist outdent indent | link image | print preview media fullpage ' +
+    '| forecolor backcolor emoticons',
+  style_formats: [
+    {title: 'Bold text', inline: 'b'},
+    {title: 'Red text', inline: 'span', styles: {color: '#ff0000'}},
+    {title: 'Red header', block: 'h1', styles: {color: '#ff0000'}},
+    {title: 'Example 1', inline: 'span', classes: 'example1'},
+    {title: 'Example 2', inline: 'span', classes: 'example2'},
+    {title: 'Table styles'},
+    {title: 'Table row 1', selector: 'tr', classes: 'tablerow1'}
+  ]
 };
 
 IMParts_Catalog['tinymce'] = {
-    instanciate: function (parentNode) {
-        var newId = parentNode.getAttribute('id') + '-e';
-        this.ids.push(newId);
-        var newNode = document.createElement('TEXTAREA');
-        newNode.setAttribute('id', newId);
-        INTERMediatorLib.setClassAttributeToNode(newNode, '_im_tinymce');
-        parentNode.appendChild(newNode);
-        this.ids.push(newId);
+  instanciate: function (parentNode) {
+    let newId = parentNode.getAttribute('id') + '-e';
+    this.ids.push(newId);
+    let newNode = document.createElement('TEXTAREA');
+    newNode.setAttribute('id', newId);
+    newNode.setAttribute('class', '_im_tinymce');
+    parentNode.appendChild(newNode);
+    this.ids.push(newId);
 
-        parentNode._im_getComponentId = (function () {
-            var theId = newId;
-            return function () {
-                return theId;
-            };
-        })();
-        parentNode._im_setValue = (function () {
-            var aNode = newNode;
-            return function (str) {
-                aNode.innerHTML = str;
-            };
-        })();
-    },
-    ids: [],
-    finish: function (update) {
-        update = (update === undefined) ? true : update;
-        if (!tinymceOption) {
-            tinymceOption = {};
-        }
-        tinymceOption.mode = 'specific_textareas';
-        tinymceOption.elements = this.ids.join(',');
-        if (update) {
-            tinymceOption.setup = function (ed) {
-                ed.on('change', (function () {
-                    var updateRquired = update;
-                    return function (ev) {
-                        if (updateRquired) {
-                            IMLibUI.valueChange(ed.id);
-                        }
-                    };
-                })());
-                ed.on('keydown', function (ev) {
-                    IMLibUI.keyDown(ev);
-                });
-                ed.on('keyup', function (ev) {
-                    IMLibUI.keyUp(ev);
-                });
-            };
-        } else {
-            tinymceOption.setup = null;
-        }
-        tinyMCE.init(tinymceOption);
-
-        for (var i = 0; i < this.ids.length; i++) {
-            var targetNode = document.getElementById(this.ids[i]);
-            var targetId = this.ids[i];
-            if (targetNode) {
-                targetNode._im_getValue = (function () {
-                    var thisId = targetId;
-                    return function () {
-                        return tinymce.EditorManager.get(thisId).getContent();
-                    };
-                })();
-            }
-        }
-
-        this.ids = [];
+    parentNode._im_getComponentId = (function () {
+      let theId = newId;
+      return function () {
+        return theId;
+      };
+    })();
+    parentNode._im_setValue = (function () {
+      let aNode = newNode;
+      return function (str) {
+        aNode.innerHTML = str;
+      };
+    })();
+  },
+  ids: [],
+  finish: function (update) {
+    update = (update === undefined) ? true : update;
+    if (!tinymceOption) {
+      tinymceOption = {};
     }
+    tinymceOption.mode = 'specific_textareas';
+    tinymceOption.elements = this.ids.join(',');
+    if (update) {
+      tinymceOption.setup = function (ed) {
+        ed.on('change', (function () {
+          let updateRquired = update;
+          return function (ev) {
+            if (updateRquired) {
+              IMLibUI.valueChange(ed.id);
+            }
+          };
+        })());
+        ed.on('keydown', function (ev) {
+          IMLibUI.keyDown(ev);
+        });
+        ed.on('keyup', function (ev) {
+          IMLibUI.keyUp(ev);
+        });
+      };
+    } else {
+      tinymceOption.setup = null;
+    }
+    tinyMCE.init(tinymceOption);
+
+    for (let i = 0; i < this.ids.length; i++) {
+      let targetNode = document.getElementById(this.ids[i]);
+      let targetId = this.ids[i];
+      if (targetNode) {
+        targetNode._im_getValue = (function () {
+          let thisId = targetId;
+          return function () {
+            return tinymce.EditorManager.get(thisId).getContent();
+          };
+        })();
+      }
+    }
+
+    this.ids = [];
+  }
 };
