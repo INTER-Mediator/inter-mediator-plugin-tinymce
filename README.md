450e426c5da30b96b2977fe7592839e00586e056
msyk <nii@msyk.net>
2021-08-15 15:48:01 +0900

    CI Integrated.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# inter-mediator-plugin-tinymce
+# inter-mediator-plugin-tinymce [![Build Status](https://github.com/INTER-Mediator/inter-mediator-plugin-tinymce/actions/workflows/node.js.yml/badge.svg)](https://github.com/INTER-Mediator/inter-mediator-plugin-tinymce/actions/workflows/node.js.yml)
 INTER-Mediator plug-in to use the tinymce.
---
+++ test.js
@@ -0,0 +1,15 @@
+/*
+ node ../node_modules/.bin/jest
+ */
+let IMParts_Catalog = {}
+
+// Thanks to https://stackoverflow.com/questions/5797852/in-node-js-how-do-i-include-functions-from-my-other-files
+const fs = require('fs');
+eval(fs.readFileSync('./index.js').toString());
+
+test('Object exists, and has fundamental methods.', () => {
+  expect(typeof IMParts_Catalog.tinymce).toBe('object')
+  expect(typeof IMParts_Catalog.tinymce.instanciate).toBe('function')
+  expect(typeof IMParts_Catalog.tinymce.finish).toBe('function')
+  }
+)
